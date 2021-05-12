2c7350a70ca84733b9621c6869ce55e4949a12a3
Alexandre Choisy <alexandre.choisy@gmail.com>
2021-05-13 00:17:54 +0200

    adding dist subtree

--- src/plugins/buefy.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue';
-import Buefy from 'buefy';
-// import '../styles/_variables.scss';
-
-Vue.use(Buefy);
--- src/plugins/index.js
+++
@@ -1,2 +0,0 @@
-export * from './buefy';
-export * from './vue-screen';
